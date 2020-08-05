b118095f00cbe81ccc4f89cf775f39776dac39c2
RubyLLL <xlyu00green@gmail.com>
2020-08-05 12:35:06 -0400

    validate user

--- functions/index.js
+++ functions/index.js
@@ -60,6 +60,17 @@ app.post('/scream', (req, res) => {
     })
 });
 
+const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(emailRegEx)) return true
+    else return false
+}
+
+const isEmpty = (str) => {
+    if(str.trim() === '') return true
+    else return false
+}
+
 // Sign up route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -67,8 +78,23 @@ app.post('/signup', (req, res) => {
         password: req.body.password,
         confirmPassword: req.body.confirmPassword,
         userHandle: req.body.userHandle
-
     };
+
+    let errors = {}
+
+    if(isEmpty(newUser.email)) errors.email = 'Must not be empty'
+    else {
+        if(!isEmail(newUser.email)) errors.email = 'Must be a valid email address'
+    }
+    
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
+
+    if(isEmpty(newUser.userHandle)) errors.userHandle = 'Must not be empty'
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
 
     // TODO: validate data
     let userToken, userID
