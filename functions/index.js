e774cc7576bc71b9abcad218c382c8aa116d63e2
RubyLLL <xlyu00green@gmail.com>
2020-08-05 18:35:44 -0400

    middleware that will ensure the user logged in before let they access some routes

--- functions/index.js
+++ functions/index.js
@@ -41,10 +41,43 @@ app.get('/screams', (req, res) => {
     .catch(e => console.error(e))
 })
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken
+    // A good practice is to start your token with 'Bearer '
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1] // take the string after Bearer 
+    } else {
+        console.error('No token found');
+        return res.status(403).json({ error: 'Unauthorized' })
+    }
+
+    // make sure the token is from our application, not somewhere else
+    // then add data into the req, so when req proceeds forward we can use token to check auth
+    admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+        req.user = decodedToken
+        console.log(decodedToken)
+        return db.collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get() // get the data with corresponding user id
+    })
+    .then(data => {
+        // data() will extract data from the document
+        req.user.userHandle = data.docs[0].data().userHandle
+        return next()
+    })
+    .catch(e => {
+        console.error('Error while verifying token ', e);
+        return res.status(403).json(e)
+    })
+}
+
+// Post one scream
+app.post('/scream', FBAuth, (req, res) => {
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.userHandle,
         createdAt: new Date().toISOString()
     };
 
