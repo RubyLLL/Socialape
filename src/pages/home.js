70f4ab8591203e10f35d865c187d2a345574a525
RubyLLL <xlyu00green@gmail.com>
2020-08-10 14:03:45 -0400

    set up basic routes

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+// rce will do this
+import React, { Component } from 'react'
+
+export class home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Home Page
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default home
