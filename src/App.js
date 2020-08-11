c574f0cae2b156d9b5aa73657d1d5c92f49af176
RubyLLL <xlyu00green@gmail.com>
2020-08-11 16:49:36 -0400

    setup redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import jwtDecode from 'jwt-decode'
 
+// Redux
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+// utils
 import themeFile from './utils/theme'
 import AuthRoute from './utils/AuthRoute'
 
@@ -13,6 +18,7 @@ import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 
+// Components
 import Navbar from './components/Navbar'
 
 const theme = createMuiTheme(themeFile)
@@ -20,6 +26,7 @@ const theme = createMuiTheme(themeFile)
 let authenticated
 // NOTE this token is stored when user logs in or signs up
 const token = localStorage.FBIdToken
+// FIXME this shit gets my window contantly reloading
 // if(token){
 //   const decodedToken = jwtDecode(token)
 //   // this token is expired
@@ -34,20 +41,22 @@ const token = localStorage.FBIdToken
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <div className='App'>
-          <Router>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={home}/>
-                <Route exact path='/login' component={login}/>
-                <Route exact path='/signup' component={signup}/>
-              </Switch>
-            </div>
-          </Router>
-        </div>
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+          <div className='App'>
+            <Router>
+              <Navbar />
+              <div className='container'>
+                <Switch>
+                  <Route exact path='/' component={home}/>
+                  <Route exact path='/login' component={login}/>
+                  <Route exact path='/signup' component={signup}/>
+                </Switch>
+              </div>
+            </Router>
+          </div>
+        </MuiThemeProvider>
+      </Provider>
       );
   }
 }
